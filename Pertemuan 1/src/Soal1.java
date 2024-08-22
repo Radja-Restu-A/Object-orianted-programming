2e81e6287e5d062a981df19b44a8cb71a179ffbc
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-22 16:03:41 +0700

    Pemberian komentar

--- Pertemuan 1/src/Soal1.java
+++ Pertemuan 1/src/Soal1.java
@@ -1,9 +1,19 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+ **/
+
 public class Soal1{
     public static void main(String[] args) {
+        /** Assignment variabel **/
         byte angka1 = 125;
         byte angka2 = 6;
+
+        /** Penjumlahan yang menyebabkan melebihi kapasitas variabel **/
         byte hasil = (byte) (angka1+angka2);
 
+        /** Print Hasil **/
         System.out.println("hasil 1 = " + hasil);
     }
 }
