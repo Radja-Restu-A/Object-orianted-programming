6e1e90d92357ef4ee02fb51d457ba493f070ec39
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-22 15:58:25 +0700

    Add files via upload

---
+++ Pertemuan 1/src/Soal1.java
@@ -0,0 +1,9 @@
+public class Soal1{
+    public static void main(String[] args) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1+angka2);
+
+        System.out.println("hasil 1 = " + hasil);
+    }
+}
---
+++ Pertemuan 1/src/Soal2.java
@@ -0,0 +1,7 @@
+public class Soal2 {
+    public static void main(String[] args) {
+        int i = 42;
+        String s = (i < 40)?"life":(i>50)?"universe":"everything";
+        System.out.println(s);
+    }
+}
