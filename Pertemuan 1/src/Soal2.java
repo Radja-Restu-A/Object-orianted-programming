b893b5379373b084bbbd078b5fca032305b306df
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-22 16:05:24 +0700

    Update komentar

--- Pertemuan 1/src/Soal2.java
+++ Pertemuan 1/src/Soal2.java
@@ -1,6 +1,15 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+ **/
+
 public class Soal2 {
     public static void main(String[] args) {
+        /** Assignment variabel **/
         int i = 42;
+
+        /** Penggunaan metode Ternary sebagai pengkondisian dibanding if**/
         String s = (i < 40)?"life":(i>50)?"universe":"everything";
         System.out.println(s);
     }
