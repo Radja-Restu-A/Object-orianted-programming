5ff75a00ea4e2cc3c8ed94c687ba2ccab63063a3
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-22 16:07:47 +0700

    update komentar

--- Pertemuan 1/src/welcome.java
+++ Pertemuan 1/src/welcome.java
@@ -1,9 +1,20 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+ **/
+
 public class welcome
 {
     public static void main(String[] args)
     {
+        /** Assignment variabel **/
         String greeting = "Welcome to Core Java!";
+
+        /** Print greeting **/
         System.out.println(greeting);
+
+        /** Loop for untuk sesuai dengan panjang greeting **/
         for (int i = 0; i < greeting.length(); i++)
             System.out.print("=");
         System.out.println();
