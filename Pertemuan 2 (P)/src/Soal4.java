ebe3ae06341befff4e9c60eaeb6ab529c6ebcf39
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-29 18:05:04 +0700

    update komentar

--- Pertemuan 2 (P)/src/Soal4.java
+++ Pertemuan 2 (P)/src/Soal4.java
@@ -1,12 +1,25 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 public class Soal4 {
+    //deklarasi function
     static short methodOne(long l) {
+        //melakukan casting
         int i = (int) l;
+        //mengcast sebelum mereturn nilai
         return (short)i;
     }
     public static void main(String[] args) {
+        //deklarasi variabel
         double d = 10.25;
+        //cast
         float f = (float) d;
+        //cast
         byte b = (byte) methodOne((long) f);
+        //menampilkan output
         System.out.println(b);
     }
 }
