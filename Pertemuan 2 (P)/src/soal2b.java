e5aad3a77f164dae1f53428889299f2f678bc632
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-29 18:01:07 +0700

    update komentar

--- Pertemuan 2 (P)/src/soal2b.java
+++ Pertemuan 2 (P)/src/soal2b.java
@@ -1,8 +1,17 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 public class soal2b {
+    //deklarasi dan assignment variabel global
     public static final double CM_PER_INCH = 2.54;
     public static void main(String[] args) {
+        //deklarasi variabel lokal
         double paperWidth = 8.5;
         double paperHeight = 11;
+        //tampilkan output
         System.out.println("Paper size in centimeters: " + paperWidth *
                 CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
     }
