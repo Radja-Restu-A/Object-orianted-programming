86570a17a3ebbb7c46cb8a07a592a9124f56b071
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-29 18:03:55 +0700

    update komentar

--- Pertemuan 2 (P)/src/Soal3.java
+++ Pertemuan 2 (P)/src/Soal3.java
@@ -1,7 +1,16 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 public class Soal3 {
     public static void main(String[] args) {
+        //deklarasi dan assignment
         double x = 92.98;
+        //melakukan cast
         int nx = (int) Math.round(x);
+        //menampilkan output
         System.out.println(nx);
     }
 }
