9c28b54da89685d843ba3d933facea3cce67becd
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-29 18:06:19 +0700

    update komentar

--- Pertemuan 2 (P)/src/Soal5.java
+++ Pertemuan 2 (P)/src/Soal5.java
@@ -1,16 +1,25 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 import java.util.Scanner;
 
 public class Soal5 {
     public static void main(String[] args) {
+        // mengambil input string pertama
         Scanner input = new Scanner(System.in);
         System.out.print("Masukan string A : ");
         String A = input.nextLine();
 
+        //mengambil input string kedua
         String capitalA = A.substring(0,1).toUpperCase();
         System.out.print("Masukan string B : ");
         String B = input.nextLine();
 
         String capitalB = B.substring(0,1).toUpperCase();
+        //menampilkan output
         System.out.println(A.length() + B.length());
 
         if(A.compareToIgnoreCase(B) > 0){
