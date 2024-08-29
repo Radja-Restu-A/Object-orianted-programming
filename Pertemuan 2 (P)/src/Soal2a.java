299aedb9709fc457c4dbd3dda10dceb902cb71e5
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-08-29 17:59:57 +0700

    update komentar

--- Pertemuan 2 (P)/src/Soal2a.java
+++ Pertemuan 2 (P)/src/Soal2a.java
@@ -1,8 +1,16 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 public class Soal2a {
     public static void main(String[] args) {
+        //deklarasi dan assignment
         final double CM_PER_INCH = 2.54;
         double paperWidth = 8.5;
         double paperHeight = 11;
+        //menampilkan output
         System.out.println("Paper size in centimeters: " +
                 paperWidth * CM_PER_INCH + " by " + paperHeight *
                 CM_PER_INCH);
