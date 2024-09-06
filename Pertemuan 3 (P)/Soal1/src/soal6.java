952209e8692d3eb70f1732dba266cde7856e21a0
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-09-06 23:05:17 +0700

    Update soal6.java

--- Pertemuan 3 (P)/Soal1/src/soal6.java
+++ Pertemuan 3 (P)/Soal1/src/soal6.java
@@ -1,12 +1,21 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Big number
+**/
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
 public class soal6 {
     public static void main(String[] args) {
+        //deklarasi
         BigInteger a, b, c;
+        //input
         Scanner input = new Scanner(System.in);
         a = input.nextBigInteger();
         b = input.nextBigInteger();
+        //algoritma utama + output
         System.out.print(a.add(b)+"\n");
         System.out.print(a.multiply(b));
     }
