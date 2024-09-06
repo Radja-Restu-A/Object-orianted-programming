7de1c7b32d090e2d683c0c5a04ee354c485e82f6
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-09-06 22:47:58 +0700

    update komentar

--- Pertemuan 3 (P)/Soal1/src/soal1.java
+++ Pertemuan 3 (P)/Soal1/src/soal1.java
@@ -1,11 +1,20 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class soal1 {
     public static void main(String[] args) {
+        //declaration
         Scanner input = new Scanner(System.in);
         System.out.print("Masukan input string : ");
+        //assignment
         String s = input.nextLine();
+        //Algoritma Pemisahan String
         StringTokenizer a = new StringTokenizer(s," !,?._'@");
         System.out.println(a.countTokens());
         while (a.hasMoreTokens()) {
