b9b0492006996be6dbb01ae6f5f7fbc80525f030
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-09-06 23:02:20 +0700

    Update soal4.java

--- Pertemuan 3 (P)/Soal1/src/soal4.java
+++ Pertemuan 3 (P)/Soal1/src/soal4.java
@@ -1,15 +1,24 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+Gaji Agent
+**/
+
 import java.util.Scanner;
 
 public class soal4 {
     public static void main(String[] args) {
+        //deklarasi dan assignment
         int item = 50000;
         byte terjual = 0;
         Scanner input = new Scanner(System.in);
         while(true){
             int gajipokok = 500000;
             int bonus = 0;
+            //input
             System.out.println("Masukan item yang terjual : ");
             terjual = input.nextByte();
+            //algoritma utama
             if (terjual >= 40){
                 bonus = (int) (terjual * (item * 0.25));
             } else if (terjual > 80) {
@@ -20,6 +29,7 @@ public class soal4 {
                 bonus = (int) (terjual * (item * 0.10));
             }
             gajipokok += bonus;
+            //output
             System.out.println("Berarti gaji pokok nya : " + gajipokok);
         }
     }
