ae8b59f3c81c544693f2a118724970fc022219e2
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-09-06 23:01:19 +0700

    Update soal3.java

--- Pertemuan 3 (P)/Soal1/src/soal3.java
+++ Pertemuan 3 (P)/Soal1/src/soal3.java
@@ -1,13 +1,22 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+Berhitung
+**/
+
 import java.util.Scanner;
 
 public class soal3 {
     public static void main(String[] args) {
+        //assignment
         Scanner input = new Scanner(System.in);
         while (true){
+            //input
             System.out.print("Masukan input : ");
             String[] temp = input.nextLine().split(" ");
             short a = Short.parseShort(temp[0]);
             Short b = Short.parseShort(temp[2]);
+            //algoritma utama
             if(a > 1000 || a < 1) {
                 System.out.print("Input diluar scoope\n");
             }else if(b > 1000 || b < 1){
@@ -26,6 +35,8 @@ public class soal3 {
                 } else if (temp[1].compareTo("%") == 0) {
                     hasil = (a % b);
                 }
+
+                //output
                 if (hasil != 0) {
                     System.out.println(hasil);
                 }else{
