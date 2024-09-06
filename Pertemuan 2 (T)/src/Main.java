aff4dfe2f024861309331dbfc1b8b84752b4f79f
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-09-06 22:44:46 +0700

    Update komentar

--- Pertemuan 2 (T)/src/Main.java
+++ Pertemuan 2 (T)/src/Main.java
@@ -1,12 +1,18 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
+    //Function hitung nilai yang sesuai dengan formula yang dibutuhkan
     static double hitungNilai(double tugas, double uts, double uas){
         return 0.20 * tugas + 0.35 * uts + 0.45 * uas;
     }
-
+    //Function Grading merupakan pengkondisian sesuai dengan niali yang diberikan
     static char grading(double nilai){
         if (nilai >= 80) {
             return 'A';
@@ -23,6 +29,7 @@ public class Main {
 
     public static void main(String[] args) {
         while(true){
+            //Declaration
             Scanner input = new Scanner(System.in);
             System.out.print("Masukan Nilai Tugas : ");
             double nilaiTugas = input.nextDouble();
@@ -32,9 +39,10 @@ public class Main {
 
             System.out.print("Masukan Nilai UAS : ");
             double nilaiUas = input.nextDouble();
-
+            
             double nilaiAkhir = hitungNilai(nilaiTugas,nilaiUts,nilaiUas);
             char grade = grading(nilaiAkhir);
+            //output
             System.out.println("Nilai akhir : " + grade + "\n");
         }
     }
