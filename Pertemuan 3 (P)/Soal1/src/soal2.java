1a7e6c6314d778d45a375eddc8a0c457b35d4fb1
Radja Restu Arsita <158986576+Radja-Restu-A@users.noreply.github.com>
2024-09-06 22:55:23 +0700

    update komentar

--- Pertemuan 3 (P)/Soal1/src/soal2.java
+++ Pertemuan 3 (P)/Soal1/src/soal2.java
@@ -1,9 +1,17 @@
+/**
+ Nama  : Radja Restu Arsita
+ Kelas : 2B - D3
+ Pengoptimalan penggunaan jenis variabel
+**/
+
 import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class soal2 {
     public static void main(String[] args) {
+        //assignment
         Scanner input = new Scanner(System.in);
+        //input
         String[] temp = input.nextLine().split(" ");
         String a = temp[0];
         int b = Integer.parseInt(temp[1]);
@@ -12,6 +20,7 @@ public class soal2 {
         int d = Integer.parseInt(temp[1]);
         temp = input.nextLine().split(" ");
         String e = temp[0];
+        //output
         int f = Integer.parseInt(temp[1]);
         System.out.println("================================");
         System.out.printf("%-15s %03d\n",a,b);
